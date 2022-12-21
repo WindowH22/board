cac9661854ffc645ff2619986a39464094cfee48
WindowH22 <dlckdgml1235@naver.com>
2022-12-21 10:46:36 +0900

    #18 - 서비스 코드 뼈대 작성
    서비스 코드에 필요한 메서드들을 명세하였다.

---
+++ src/main/java/com/fastcampus/board/service/ArticleService.java
@@ -0,0 +1,40 @@
+package com.fastcampus.board.service;
+
+import com.fastcampus.board.domain.type.SearchType;
+import com.fastcampus.board.dto.ArticleDto;
+import com.fastcampus.board.dto.ArticleUpdateDto;
+import com.fastcampus.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ArticleDto> searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
