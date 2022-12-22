33462918f8e44218a772a05588bef098a9c9d468
WindowH22 <dlckdgml1235@naver.com>
2022-12-22 14:18:46 +0900

    #18 - 테스트 수정
    articleRepositroy.save의 매개변수 변경으로 인해 매개변수를 변경하였음

--- src/test/java/com/fastcampus/board/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/board/repository/JpaRepositoryTest.java
@@ -59,7 +59,7 @@ class JpaRepositoryTest {
         long previousCount =  articleRepository.count();
 
         //When
-        Article savedArticle = articleRepository.save(Article.of("new article","new content","spring"));
+        Article savedArticle = articleRepository.save(Article.of(null,"new content","spring"));
         //Then
         assertThat(articleRepository.count()).isEqualTo(previousCount+1);
     }
